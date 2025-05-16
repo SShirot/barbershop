2e17befcd2d53aea6038d291cd0904621ea69255
Nguyễn Khắc Huy <97592983+SShirot@users.noreply.github.com>
2025-05-16 23:00:14 +0700

    update
    s

--- api/src/services/admin/menuService.js
+++ api/src/services/admin/menuService.js
@@ -1,4 +1,4 @@
-const MenuRepository = require('../../repositories/MenuRepository');
+const MenuRepository = require('../../repositories/menuRepository');
 
 const MenuService = {
     getAll: async (page, pageSize, name) => {
